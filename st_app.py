d2575aa4cafd2923a0bb9be05e7d5058670a0cb6
Terje <terje.ambjornsen@telenor.com>
2025-09-04 19:22:45 +0000

    changed report name

--- st_app.py
+++ st_app.py
@@ -5,6 +5,8 @@ st.header("Hello World")
 st.warning("This is a template app. Please change me!")
 
 pg = st.navigation(
-    [st.Page("gui_pages/page1.py", title="Home", default=True), st.Page("gui_pages/page2.py", title="Page 2")]
+    [st.Page("gui_pages/page1.py", title="Home", default=True), 
+     st.Page("gui_pages/page2.py", title="Page 2"), 
+     st.Page("gui_pages/ssb_report.py", title="SSB report")],
 )
 pg.run()
