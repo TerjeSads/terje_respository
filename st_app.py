22ec2e2835e8927c0590cbcbe0060efa804a9b04
Terje <terje.ambjornsen@telenor.com>
2025-11-24 21:24:20 +0000

    updates

--- st_app.py
+++ st_app.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="ADS Template App", layout="wide")
 pg = st.navigation(
     [   st.Page("gui_pages/auction_simulator.py", title="Auction Simulator"),
         st.Page("gui_pages/geo_tn_vula_arpu.py", title="TN and VULA ARPU Data"),
+        st.Page("gui_pages/gsmai_data_collector.py", title="GSMAI Data Collector"),
     ]
 )
 pg.run()
